c659775a61710e3fa6615d5c61e665167f6a35a6
unknown <mjackson@.(none)>
2009-03-25 14:34:59 -0400

    Fixing issue where headers were being placed in the wrong directory during installation

--- Resources/CMake/MXA_CMakeMacros.cmake
+++ Resources/CMake/MXA_CMakeMacros.cmake
@@ -2,7 +2,7 @@
 MACRO (MXA_SOURCE_PROPERTIES NAME HEADERS SOURCES)
 
   INSTALL (FILES ${HEADERS}
-              DESTINATION include/R3D/${NAME}
+              DESTINATION include/MXA/${NAME}
               COMPONENT Headers           
   )
 
