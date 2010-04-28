867d501036e5379f6916d2c87245a51cd61c7d6d
Michael Jackson <mike.jackson@bluequartz.net>
2010-04-28 12:25:18 -0400

    Fixing several issues that were preventing ParaView 3.8 RC1 from properly configuring on Windows by fixing an cmake variable that dictates if the MXADataModel project should attempt to install its own files. There was a type with the original cmake variable which was fixed in this commit

--- Resources/CMake/ParaViewPluginIncludeMXADataModel.cmake
+++ Resources/CMake/ParaViewPluginIncludeMXADataModel.cmake
@@ -9,7 +9,7 @@ SET(HDF5_LIBRARY  ${PARAVIEW_HDF5_LIBRARIES} CACHE FILEPATH "" )
 
 INCLUDE_DIRECTORIES( ${HDF5_INCLUDE_DIR} )
 
-# ------- Add support for Expat and XML parsing
+# ------- Add support for Expat and XML parsing using the Expat that comes with ParaView/VTK
 SET (MXA_USE_XML  ON)
 SET (MXA_EXTERNAL_EXPAT_LIBRARY vtkexpat)
 SET (EXPAT_FOUND 1)
@@ -19,18 +19,32 @@ SET (EXPAT_INCLUDE_DIRS "${ParaView_SOURCE_DIR}/VTK/Utilities/vtkexpat")
 # ---- Remove support for Tiff image functionality
 SET (MXA_USE_TIFF OFF)
 
+#-- Remove MXA Examples from being built
 SET (MXA_BUILD_EXAMPLES OFF CACHE BOOL "" )
+
+#-- Remove the MXA Testing from being built
 SET (MXA_BUILD_TESTING FALSE)
 
+#-- Set the Name of the MXA Library. This should ALWAYS be MXADataModel
 SET (MXADATAMODEL_LIB_NAME MXADataModel)
-SET (MXA_USE_DEBUG_LIBRARY_NAME FALSE)
-set (MXA_BOOST_HEADERS_ONLY TRUE)
 
+#-- ParaView does not like it when we use specific Debug suffixes for library names
+#-- so turn it off here
+SET (MXA_USE_DEBUG_LIBRARY_NAME FALSE)
+
+#-- We are NOT going to install the default set of files from the MXA Distribution
 set (MXA_INSTALL_FILES 0 CACHE INTERNAL "Ignore the MXA Install Rules, defaulting to the enclosing projects install rules")
 set (MXA_BUILD_TESTING_SAVE ${MXA_BUILD_TESTING})
-#SET (MXA_BUILD_TESTING ON)
+
+#-- ParaView already defines where to place output files like libraries and executables
 SET (MXA_OUTPUT_DIRS_DEFINED 1)
+
+#-- We need some more boost libraries?
 set (MXA_BOOST_HEADERS_ONLY 0)
+
+# --------------------------------------------------------------------
+#-- Build the MXADataModel library
+# --------------------------------------------------------------------
 ADD_SUBDIRECTORY("${MXADataModel_SOURCE_DIR}" ${ParaView_BINARY_DIR}/${MXADATAMODEL_LIB_NAME})
 INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS})  # Include the Boost Headers
 SET (MXA_BUILD_TESTING ${MXA_BUILD_TESTING_SAVE})
@@ -41,8 +55,10 @@ MARK_AS_ADVANCED(MXA_BUILD_WITH_INSTALL_NAME)
 SET (MXADATAMODEL_TARGET_ADDED "1" CACHE INTERNAL "MXADataModel Added to ParaView as a libary")
 MESSAGE (STATUS "[${MODULE_NAME}] Added the MXADataModel library.")
 
+#-- Set some inlucde directories that will be needed later on in the Build
 INCLUDE_DIRECTORIES (${MXADataModel_SOURCE_DIR}/Source)
 INCLUDE_DIRECTORIES (${ParaView_BINARY_DIR}/MXADataModel)
 
+#-- Include a CMake file with some Macros that will be needed.
 include (${MXADataModel_SOURCE_DIR}/Resources/CMake/CMakeMacros.cmake)
 MXA_LIBRARIES_INSTALL_RULES ("hdf5" "bin")
