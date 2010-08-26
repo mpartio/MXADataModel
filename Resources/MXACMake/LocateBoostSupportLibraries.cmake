63ba84b0290678d7e0e4ce15ea96f66db10b2bb9
Mike Jackson <mike.jackson@bluequartz.net>
2010-08-26 14:22:59 -0400

    Moving files to the cmp git submodule

--- Resources/MXACMake/LocateBoostSupportLibraries.cmake
+++ Resources/MXACMake/LocateBoostSupportLibraries.cmake
@@ -8,7 +8,7 @@ SET (Boost_FIND_REQUIRED TRUE)
 SET (Boost_FIND_QUIETLY TRUE)
 set (Boost_USE_MULTITHREADED TRUE)
 set (Boost_USE_STATIC_LIBS TRUE)
-SET (Boost_ADDITIONAL_VERSIONS "1.36" "1.36.0" "1.41" "1.41.0" "1.39" "1.39.0")
+SET (Boost_ADDITIONAL_VERSIONS "1.44" "1.44.0" "1.36" "1.36.0" "1.41" "1.41.0" "1.39" "1.39.0")
 
 # --------------------------------------------------------------------
 # MXA_BOOST_HEADERS_ONLY determines if any boost libraries are going to
@@ -16,7 +16,7 @@ SET (Boost_ADDITIONAL_VERSIONS "1.36" "1.36.0" "1.41" "1.41.0" "1.39" "1.39.0")
 # libraries are required but if you choose to build the ImportGenerator
 # program then you will need the boost program_options library.
 if ( NOT MXA_BOOST_HEADERS_ONLY)
- set (MXA_BOOST_COMPONENTS program_options )
+ set (MXA_BOOST_COMPONENTS "" )
 endif()
 
 FIND_PACKAGE(Boost COMPONENTS  ${MXA_BOOST_COMPONENTS} )
